30f22742dd1591643561ae78eb2a5e9514a3eb39
Mattia Procopio <promat85@gmail.com>
2019-08-01 16:15:05 +0200

    Refactor to CLOS

--- socket.lisp
+++ socket.lisp
@@ -17,60 +17,73 @@
   (sin-addr :unsigned-long)
   (sin-zero :string))
 
-(defvar main-fd nil)
-(defvar child-fd nil)
-
-  
-(defun create-socket (domain socket-type)
-  (setq main-fd (foreign-funcall "socket" :int domain :int socket-type :int 0 :int)))
-
-(defun close-socket (file-descriptor)
-  (foreign-funcall "close" :int file-descriptor :int))
-
 (defun inet-addr (addr)
   (foreign-funcall "inet_addr" :string addr :long))
 
 
-(defun bind (fd address port)
+(defclass socket ()
+  ((address
+    :initarg :address
+    :initform "127.0.0.1"
+    :accessor address)
+   (port
+    :initarg :port
+    :initform nil
+    :accessor port)
+   (fd
+    :accessor fd)
+   (domain
+    :initform 2 ; AF_INET
+    :accessor domain)
+   (socket-type
+    :initform 1 ; SOCK_STREAM
+    :accessor socket-type)
+   (backlog
+    :initform 5
+    :accessor backlog)
+   (child-fd
+    :accessor child-fd)))
+
+(defmethod create-socket ((obj socket))
+  (setf (slot-value obj 'fd)
+	(foreign-funcall "socket" :int (domain obj) :int (socket-type obj) :int 0 :int)))
+
+(defmethod close-socket ((obj socket))
+  (foreign-funcall "close" :int (fd obj) :int)
+  (foreign-funcall "close" :int (child-fd obj) :int))
+
+(defmethod bind ((obj socket))
   (with-foreign-object (addr '(:struct socket-addr-in))
-    ;; Init the slots
-    (setf (foreign-slot-value addr '(:struct socket-addr-in) 'sin-family) 2)
-    (setf (foreign-slot-value addr '(:struct socket-addr-in) 'sin-addr) (inet-addr address))
-    (setf (foreign-slot-value addr '(:struct socket-addr-in) 'sin-port) (foreign-funcall "htons" :int port :int))
+    (setf (foreign-slot-value addr '(:struct socket-addr-in) 'sin-family) (domain obj))
+    (setf (foreign-slot-value addr '(:struct socket-addr-in) 'sin-addr) (inet-addr (address obj)))
+    (setf (foreign-slot-value addr '(:struct socket-addr-in) 'sin-port) (foreign-funcall "htons" :int (port obj) :int))
     (with-foreign-slots ((sin-family sin-port sin-addr) addr (:struct socket-addr-in))
       (format t "Family: ~a Port: ~a Address: ~a" sin-family sin-port sin-addr))
     (foreign-funcall "bind"
-		     :int fd
+		     :int (fd obj)
 		     :pointer addr
 		     :int (foreign-type-size '(:struct socket-addr-in))
 		     :int)))
 
-
-(defun socket-listen (fd backlog)
+(defmethod socket-listen ((obj socket))
   (foreign-funcall "listen"
-		   :int fd
-		   :int backlog
+		   :int (fd obj)
+		   :int (backlog obj)
 		   :int))
 
-(defun accept-connection (fd)
+(defmethod accept-connection ((obj socket))
   (with-foreign-objects ((client '(:struct socket-addr-in))
 			 (len :int))
-    (setq child-fd (foreign-funcall "accept"
-				    :int fd
-				    :pointer client
-				    :pointer len
-				    :int))
-    (with-foreign-pointer-as-string (buf 4096)
-      (foreign-funcall "read"
-		       :int child-fd
-		       :pointer buf
-		       :int 4096
-		       :int)buf))))
+    (setf (slot-value obj 'child-fd) (foreign-funcall "accept"
+						      :int (fd obj)
+						      :pointer client
+						      :pointer len
+						      :int))))
 
-(defun start-server (address port &optional (backlog 5))
-  (create-socket 2 1)
-  (bind main-fd address port)
-  (socket-listen main-fd backlog)
-  (accept-connection main-fd)
-  (close-socket main-fd)
-  (close-socket child-fd))
+(defmethod recv ((obj socket))
+  (with-foreign-pointer-as-string (buf 4096)
+    (foreign-funcall "read"
+		     :int (child-fd obj)
+		     :pointer buf
+		     :int 4096
+		     :int)buf)))
